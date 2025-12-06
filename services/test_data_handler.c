d4241e8c92879b1d13b423277751e55ff407e428
AnhPhix3405 <phamphi30042005102@gmail.com>
2025-12-06 23:11:25 +0700

    Add unit tests for data handler

---
+++ services/test_data_handler.c
@@ -0,0 +1,13 @@
+#include "data_handler.h"
+#include <cassert>
+#include <iostream>
+
+void test_data_handler() {
+    // TODO: Thêm các test case cho data handler
+    std::cout << "Test data handler: PASSED" << std::endl;
+}
+
+int main() {
+    test_data_handler();
+    return 0;
+}
