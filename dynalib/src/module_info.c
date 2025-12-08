47c365a11886b1973a4f71f39d1f6fe3c334c9fc
AnhPhix3405 <phamphi30042005102@gmail.com>
2025-12-08 22:48:38 +0700

    Update module info data structure

---
+++ dynalib/src/module_info.c
@@ -0,0 +1,10 @@
+#include "module_info.h"
+
+module_info_t global_module_info = {
+    .platform_id = 123,
+    .version = 1,
+    .module_type = 2,
+    .dependencies = 0,
+    .build_timestamp = 20251208,
+    .flags = 0x01
+};
