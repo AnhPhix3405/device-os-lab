4d856ef162f2e3c2b9e1493c69d6da8c09933bec
AnhPhix3405 <phamphi30042005102@gmail.com>
2025-12-11 21:43:41 +0700

    Add unit tests for GSM0710 muxer

--- gsm0710muxer/gsm0710muxer.cpp
+++ gsm0710muxer/gsm0710muxer.cpp
@@ -13,6 +13,7 @@
 void GSM0710Muxer::initialize() {
     if (buffer == nullptr) {
         buffer = new char[bufferSize]; // Allocate memory
+        std::cout << "GSM0710Muxer initialized with buffer size: " << bufferSize << std::endl;
     }
 }
 
@@ -20,9 +21,11 @@ void GSM0710Muxer::cleanup() {
     if (buffer != nullptr) {
         delete[] buffer; // Free allocated memory
         buffer = nullptr;
+        std::cout << "GSM0710Muxer buffer cleaned up." << std::endl;
     }
 }
 
 GSM0710Muxer::~GSM0710Muxer() {
     cleanup();
+    std::cout << "GSM0710Muxer destroyed." << std::endl;
 }
